d6c453fd8aa7f68970052056a96be4cdd108a27a
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-12-05 21:21:25 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,35 @@
+function forLoop(array){
+  for( var i = 0; i < 25; i++){
+    
+      if (i === 1){
+      array.push(`I am ${i} strange loop.`)
+      }else{
+        array.push(`I am ${i} strange loops.`)
+      }
+  }
+  return array
+}
+
+function whileLoop(number){
+  
+  while (number > 0){
+    console.log(--number)
+  }
+  return 'done'
+}
+
+whileLoop(6)
+
+
+function incrementVariable() {
+  var i = 10;
+  i = i - 1;
+}
+
+function doWhileLoop(array){
+  do{
+    incrementVariable();
+  }while (array.length > 0)
+  
+  return array;
+}
