f21bfae026089c85da42cf45dabcb740f6185629
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-12-05 04:23:55 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+function forLoop(array){
+  array = [];
+  for( var i = 0; i <= 25; i++){
+    
+      if (i === 1){
+      array = [`I am ${i} strange loop.`]
+      }else{
+        array = [`I am ${i} strange loops.`]
+      }
+       return array
+  }
+}
+
+
+forLoop([])
